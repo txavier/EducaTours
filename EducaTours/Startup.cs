93bafeff9a9d750c44832182bea916d72afab2af
txavier <txavier@gmail.com>
2015-04-02 17:23:14 -0400

    - Initial commit

---
+++ EducaTours/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(EducaTours.Startup))]
+
+namespace EducaTours
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
